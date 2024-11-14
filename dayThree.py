8a5222b34986e5df5e7270757399e8c32ff0a57a
binga17 <bigyan.bashyal@gmail.com>
2024-11-13 22:19:50 -0600

    Create dayThree.py
    day 3

---
+++ dayThree.py
@@ -0,0 +1,9 @@
+#function
+australia = 11
+southAfrica = 11
+india = 11
+
+def cricketPlayers():
+    totalCricketPlayers = australia + southAfrica + india
+    print(totalCricketPlayers)
+cricketPlayers()
