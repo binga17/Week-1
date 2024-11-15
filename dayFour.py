4cf20e9d173692081d8ab211500ed576b5d051b2
binga17 <bigyan.bashyal@gmail.com>
2024-11-14 22:24:21 -0600

    Create dayFour.py
    lists

---
+++ dayFour.py
@@ -0,0 +1,18 @@
+#Task 1: List creation
+
+fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"]
+print(fruits)
+
+#Task 2: Accessing elements
+
+print(fruits[0])
+print(fruits[-1])
+
+#Task 3: Modifying Lists
+
+fruits[1] = "Blueberry"
+
+#Adding items to the list
+
+fruits.append("fig")
+print(fruits)
